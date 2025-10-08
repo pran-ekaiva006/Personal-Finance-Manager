78060f98129ce06aa71e40b489590732a1d4d928
Pranjal Kumar Verma <pranjalverma975@gmail.com>
2025-10-09 00:05:28 +0530

    chg

--- server/start.js
+++ server/start.js
@@ -1,4 +1,10 @@
 import dotenv from 'dotenv';
+import dns from 'dns';
+
+// Force DNS resolution to prefer IPv4. This is a common fix for
+// ENETUNREACH errors in cloud environments like Render.
+dns.setDefaultResultOrder('ipv4first');
+
 dotenv.config();
 
 import('./index.js');
